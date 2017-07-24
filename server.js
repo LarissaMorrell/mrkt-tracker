f700c6871ab94a1c83a1d8f8fee203e6d42382c9
LarissaMorrell <larissa@larissamorrell.com>
2017-07-24 16:54:50 -0400

    added employees

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // routes ======================================================================
 require('./app/routes/passport.js')(app, passport); // load our routes and pass in our app and fully configured passport
 require('./app/routes/store.js')(app, passport);
+require('./app/routes/employee.js')(app, passport);
 
 // launch ======================================================================
 app.listen(port);
