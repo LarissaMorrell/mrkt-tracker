e2fa8e65542a7e28d7a0bc9b3ad9ff05ac5cf7f5
LarissaMorrell <larissa@larissamorrell.com>
2017-08-07 15:38:47 -0400

    updated header to correct links added fa-fw class for spacing after fa added header and footer to profile page changed header colors

--- public/app.js
+++ public/app.js
@@ -1,6 +1,15 @@
-console.log('in app.js');
+
+
+
+
+
 
 $(document).on('click', '#edit-store-location', function(event) {
   console.log('in event listener');
     $('.toggle-details').toggle();
+});
+
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
 });
