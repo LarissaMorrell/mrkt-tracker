992930c53da6edd0a14470e5e77b7073244900ba
LarissaMorrell <larissa@larissamorrell.com>
2017-08-17 11:45:30 -0400

    fixed padding of store-boxes

--- public/app.js
+++ public/app.js
@@ -1,15 +1,4 @@
-
-
-
-
-
-
 $(document).on('click', '#edit-store-location', function(event) {
   console.log('in event listener');
     $('.toggle-details').toggle();
-});
-
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
 });
