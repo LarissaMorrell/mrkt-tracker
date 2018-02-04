ac21676b9e78ab7dfa138d5264df026bb5fbb546
LarissaMorrell <larissa@larissamorrell.com>
2018-02-04 16:50:08 -0500

    fixed deprecation warnings

--- server.js
+++ server.js
@@ -17,13 +17,17 @@ require("./config/passport")(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan("dev")); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+// app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs"); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: process.env.SECRET })); // session secret
+app.use(
+  session({ secret: process.env.SECRET, resave: true, saveUninitialized: true })
+); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
